35383084a4d302854dfd2f7e139c033a36e4b101
kjo124 <kylejodin@gmail.com>
2020-12-01 18:57:05 -0700

    end of day commint, trying to open html

--- code/pagesearcher.py
+++ code/pagesearcher.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import html2text
+import urllib.request
+
+url = "http://news.bbc.co.uk/2/hi/health/2284783.stm"
+
+response = urllib.request.urlopen(url)
+html = response.read()
+
+
+print(html)
