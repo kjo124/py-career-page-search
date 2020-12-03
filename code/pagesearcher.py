8d6bf632cf893ff279fb8026a92bec6818a0f474
kjo124 <kylejodin@gmail.com>
2020-12-02 19:30:44 -0700

    updated to use beautifulsoup, look into documenation later

--- code/pagesearcher.py
+++ code/pagesearcher.py
@@ -1,10 +1,15 @@
-import html2text
 import urllib.request
+from bs4 import BeautifulSoup
 
-url = "http://news.bbc.co.uk/2/hi/health/2284783.stm"
+url = "https://boards.greenhouse.io/paytronix/jobs/2471269?gh_src=2c6509dd1us"
 
 response = urllib.request.urlopen(url)
 html = response.read()
+soup = BeautifulSoup(html, 'html.parser')
 
 
-print(html)
+# python3.9 /Users/kyleodin/Documents/GitHub/py-career-page-search/code/pagesearcher.py
+
+# Documentation: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+# print(soup.get_text())
+# print(soup.prettify())
