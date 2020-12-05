903777ea8edbf3c21eaab6735b7f72a6d2ed837f
kjo124 <kylejodin@gmail.com>
2020-12-04 18:54:42 -0700

    planning and example files to play around with

--- code/pagesearcher.py
+++ code/pagesearcher.py
@@ -1,15 +1,27 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-url = "https://boards.greenhouse.io/paytronix/jobs/2471269?gh_src=2c6509dd1us"
+url = "https://boards.greenhouse.io/paytronix/jobs/2332500?gh_src=0e280c641us"
 
 response = urllib.request.urlopen(url)
 html = response.read()
 soup = BeautifulSoup(html, 'html.parser')
 
 
-# python3.9 /Users/kyleodin/Documents/GitHub/py-career-page-search/code/pagesearcher.py
+# to run: python3.9 /Users/kyleodin/Documents/GitHub/py-career-page-search/code/pagesearcher.py
 
-# Documentation: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-# print(soup.get_text())
+# Documentation of soup: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+print(soup.get_text())
 # print(soup.prettify())
+
+# assign text to jobs
+# create full text of all jobs with only spaces between all terms
+# create html from full text of jobs
+# open webpage in chrome
+# contrl-a and copy categoriesraw from filters tab
+# open glossary tab and control-a copy Glossary
+# create a dicationary of terms
+# go back to fill text file and count all terms from dicationary
+# arange in assending order
+# assign terms to jobs
+# evaluate for to long term goals to inprove on
