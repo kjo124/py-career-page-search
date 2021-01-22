f1e3a9135389b455ade420bf10ef843ddff20634
kjo124 <kylejodin@gmail.com>
2021-01-21 19:35:36 -0700

    careerpagesaver and jobsearchbyskill should be done

--- code/jobsearchbyskill.py
+++ code/jobsearchbyskill.py
@@ -1,9 +1,8 @@
-import urllib.request
 from bs4 import BeautifulSoup
 import mysql.connector
-from pathlib import Path
-import re
 import os
+from shutil import copyfile
+from pathlib import Path
 
 mySQLpasswordFile = open(
     "/Users/kyleodin/Documents/GitHub/py-career-page-search/MySQLpassword.txt", "r")
@@ -29,22 +28,28 @@ htmlFiles = []
 def getAllHTMLfilesInDirectory(directory):
     for entry in os.scandir(directory):
         if not os.path.isdir(entry.path):
-            htmlFiles.append(entry.path)
+            if "Career Page.html" not in str(entry):
+                htmlFiles.append(entry.path)
         else:
             getAllHTMLfilesInDirectory(entry.path)
 
 
-def copyMatchingHTMLfiles(files, searchTerm):
-    for file in files:
-        with open(file) as f:
-            soup = BeautifulSoup(f.read(), 'html.parser')
-            fileText = soup.get_text()
-            fileText = fileText.replace('\n', '').replace(' ', '').lower()
-            searchTerm = searchTerm.replace('\n', '').replace(' ', '').lower()
-            if searchTerm in fileText:
-                # copy HTML and place into a searchTerm directory
-                print("matching file:" + file)
+def copyMatchingHTMLfiles(files, searchTerms):
+    for term in searchTerms:
+        path = "/Users/kyleodin/Documents/GitHub/py-career-page-search/TermSearching/" + term + "/"
+        Path(path).mkdir(parents=True, exist_ok=True)
+        for file in files:
+            with open(file) as f:
+                soup = BeautifulSoup(f.read(), 'html.parser')
+                fileText = soup.get_text()
+                fileText = fileText.replace('\n', '').replace(' ', '').lower()
+                term = term.replace('\n', '').replace(' ', '').lower()
+                if term in fileText:
+                    print("matching file:" + file)
+                    copyfile(
+                        file, "/Users/kyleodin/Documents/GitHub/py-career-page-search/TermSearching/" + term + "/" + os.path.basename(file))
 
 
+searchTerms = ["java", "sql"]
 getAllHTMLfilesInDirectory(directoryPath)
-copyMatchingHTMLfiles(htmlFiles, "javA")
+copyMatchingHTMLfiles(htmlFiles, searchTerms)
